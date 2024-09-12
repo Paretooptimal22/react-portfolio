9ef2ce5893b52a2a94eb06e6a5e5fc77fe09fb9b
kevin young <you.k2208@gmail.com>
2024-09-12 13:25:13 +0900

    set up for tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html',  // include this if using Vite
+    './src/**/*.{js,jsx,ts,tsx}', // for React components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
