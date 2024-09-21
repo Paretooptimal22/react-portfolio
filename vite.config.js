e7e4af975329759f3940f0b1368108983ad22008
kevin young <you.k2208@gmail.com>
2024-09-21 01:20:18 -0700

    added conditional on base vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   server: {
     open: true // Automatically opens the browser when you run the dev server
   },
   build: {
     outDir: 'build' // Use 'build' to keep consistency with Create React App
-  }
-});
+  },
+  base: command === 'build' ? '/your-repo-name/' : '/', // Use '/your-repo-name/' for GitHub Pages and '/' for local dev
+
+}));
